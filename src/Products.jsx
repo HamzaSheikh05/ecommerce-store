09af3ed851ac2ef63f5ccfdd46fb382f4e84460c
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-28 21:01:42 +0400

    Simplified App.js and created Products.jsx to have all the products

---
+++ src/Products.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState} from "react";
+import Spinner from "./Spinner";
+import useFetch from "./services/useFetch";
+
+export default function Products() {
+  // Declare a state
+  const [size, setSize] = useState("");
+
+  const {data: products, loading, error} = useFetch("products?category=shoes");
+
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
+  // look through the list of products and filter out any that do not have selected size
+  const filteredProducts = size ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
+  : products;
+
+  if (error) throw error;
+  if (loading) return <Spinner/>
+
+  return (
+    <>
+          <section id="filters">
+            <label htmlFor="size">Filter by Size:</label>{" "}
+            <select id="size" value={size} onChange={(e) => setSize(e.target.value)}>
+              <option value="">All sizes</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+            </select>
+            {size && <h2>Found {filteredProducts.length} Items</h2>}
+          </section>
+          <section id="products">{filteredProducts.map(renderProduct)}</section>
+    </>
+  );
+}

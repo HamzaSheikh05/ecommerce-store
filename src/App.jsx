b750f6a66a1d53a3d8c689d81483b8e6580d9a8e
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-28 19:59:09 +0400

    Created a error handling component

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,18 @@ import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import { useState } from "react";
-
-const products = [
-  {
-    "id": 1,
-    "category": "shoes",
-    "image": "shoe1.jpg",
-    "name": "Hiker",
-    "price": 94.95,
-    "skus": [
-      { "sku": "17", "size": 7 },
-      { "sku": "18", "size": 8 }
-    ],
-    "description": "This rugged boot will get you up the mountain safely."
-  },
-  {
-    "id": 2,
-    "category": "shoes",
-    "image": "shoe2.jpg",
-    "name": "Climber",
-    "price": 78.99,
-    "skus": [
-      { "sku": "28", "size": 8 },
-      { "sku": "29", "size": 9 }
-    ],
-    "description": "Sure-footed traction in slippery conditions."
-  },
-  {
-    "id": 3,
-    "category": "shoes",
-    "image": "shoe3.jpg",
-    "name": "Explorer",
-    "price": 145.95,
-    "skus": [
-      { "sku": "37", "size": 7 },
-      { "sku": "38", "size": 8 },
-      { "sku": "39", "size": 9 }
-    ],
-    "description": "Look stylish while stomping in the mud."
-  }
-];
+import { useState, useEffect } from "react";
+import {getProducts} from "./services/productService";
 
 export default function App() {
   // Declare a state
   const [size, setSize] = useState("");
+  const [products, setProducts] = useState([]);
+
+  //Declare useEffect
+  useEffect(() => {
+    getProducts("shoes").then((response) => setProducts(response));
+  }, [])
 
   function renderProduct(p) {
     return (
@@ -75,8 +42,9 @@ export default function App() {
               <option value="8">8</option>
               <option value="9">9</option>
             </select>
+            {size && <h2>Found {filteredProducts.length} Items</h2>}
           </section>
-          <section id="products">{products.map(renderProduct)}</section>
+          <section id="products">{filteredProducts.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />

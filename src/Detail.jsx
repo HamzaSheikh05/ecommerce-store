d0aacf2faaa130ec1c7ade1379a89561f0a8b4d1
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-28 22:24:16 +0400

    Display 404 error if page is missing and added add to cart button and onclick routing to cart

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import PageNotFound from './PageNotFound';
 import useFetch from './services/useFetch';
 import Spinner from './Spinner';
 
 export default function Detail(){
     const {id} = useParams();
+    const navigate = useNavigate();
     const {data: product, loading, error} = useFetch(`products/${id}`);
     if (loading) return <Spinner/>
+    if (!product) return <PageNotFound/>
     if (error) throw error;
 
     return(
@@ -15,6 +17,9 @@ export default function Detail(){
             <h1>{product.name}</h1>
             <p>{product.description}</p>
             <p id='price'>${product.price}</p>
+            <p>
+                <button className='btn btn-primary' onClick={() => navigate('/cart')}>Add to Cart</button>
+            </p>
             <img src={`/images/${product.image}`} alt={product.category} />
         </div>
     );
